721e13ae2ac45f57e7cbec2d8e518d8c03882d75
Sushi <Sushi@192.168.1.21>
2017-03-05 14:40:09 +0100

    commit

--- workspace/BASmidi/src/Main.java
+++ workspace/BASmidi/src/Main.java
@@ -24,7 +24,7 @@ import javax.sound.midi.MidiDevice.Info;
 public class Main {
 
 	public static void main(String[] args) throws InvalidMidiDataException, IOException, MidiUnavailableException{
-		
+		//yeah
 		Choisir();
 		
 	}
