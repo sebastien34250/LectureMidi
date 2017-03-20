4ac10415fc3e1278131962327bc7daf321491485
EV <elisabeth_vas@hotmail.com>
2017-03-20 13:13:25 +0100

    pause et reprise en boucle

--- workspace/BASmidi/src/Pause.java
+++ workspace/BASmidi/src/Pause.java
@@ -10,8 +10,10 @@ import javax.sound.midi.Sequencer;
 
 public class Pause {
 	// La boucle est en train d'être lue, l'utilisateur la met sur pause.
-	private static final int LOOP_CONTINUOUSLY = -1;
+	//private static final int LOOP_CONTINUOUSLY = -1;
+	
 	private Lecture lec;
+	
 	
 	public Pause(Lecture lect){
 		this.lec=lect;
@@ -20,32 +22,31 @@ public class Pause {
 		Scanner sc = new Scanner(System.in);
 		
 		String rep= sc.nextLine();
-//		lec.sequencer.setLoopCount(4);
-		
-while(relecture){			
+
 	
-		
+	
+while(relecture){		
 		if(rep.equals("p")){
 			
 			lec.sequencer.stop();
+		
+		
 			
-			lec.sequencer.setLoopStartPoint(lect.sequence.getTickLength());
 			
 			System.out.println("pour reprendre appuyer sur la lettre 'o' ?");
 			//Scanner sc2 = new Scanner(System.in);
 			String rep2 = sc.nextLine();
 
 			if(rep2.equals("o")){
-				
-				
-				//lec.sequencer.getLoopStartPoint();
-
+			
+				lec.sequencer.setTickPosition(lec.sequencer.getTickPosition());
+			//	lec.sequencer.setLoopStartPoint(lec.sequencer.getTickPosition());
 				lec.sequencer.start();
 				
-
-				
+			
 				System.out.println("appuyer p pour mettre pause");
 				rep= sc.nextLine();
+			
 			}
 		
 			
@@ -55,10 +56,10 @@ while(relecture){
 		System.out.println("lecture du morceau terminé");
 			relecture= false;
 		}
-}
+
 	}
 		
-	
+	}
 
 	
 }
